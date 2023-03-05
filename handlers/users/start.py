2d1272e73156fac63003e91132aac2ebe5680a7c
Nurmuhammad <sharobiddinovnurmuhammad23@gmail.com>
2023-03-05 18:10:51 +0500

    translate bot admin panel commit

--- handlers/users/start.py
+++ handlers/users/start.py
@@ -1,8 +1,9 @@
+import sqlite3
 from aiogram import types
 from aiogram.dispatcher.filters.builtin import CommandStart
 from keyboards.default.startmenu import menustart, admin_menustart
 from data.config import ADMINS
-from loader import dp, db
+from loader import dp, db, bot
 
 @dp.message_handler(CommandStart(), user_id=ADMINS[0])
 async def bot_start_admin(message: types.Message):
@@ -12,12 +13,10 @@ async def bot_start_admin(message: types.Message):
 async def bot_start(message: types.Message):
     await message.answer(f"Salom, {message.from_user.full_name}!", reply_markup=menustart)
     try:
-        db.add_user(message.from_user.full_name, message.from_user.id)
-        await message.bot.send_message(chat_id=ADMINS[0],
-                                       text=f"{message.from_user.full_name} botga qo'shildi!"
-                                       )
-    except:
-        pass
+        db.add_user(id=message.from_user.id,
+                    name=message.from_user.full_name)
+    except sqlite3.IntegrityError as err:
+        await bot.send_message(chat_id=ADMINS[0], text=err)
 @dp.message_handler(text='👥Foydalanuvchilar󠁧󠁢', user_id=ADMINS[0])
 async def bot_users(message: types.Message):
     users = db.select_all_users()
